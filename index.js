60780958e096a7deb5daf8ab8b92b1f07d31e339
spurtilo <148346961+spurtilo@users.noreply.github.com>
2025-10-03 13:59:42 +0300

    Build routes, add middleware

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ let recipes = require("./data/recipes");
 const app = express();
 const morgan = require("morgan");
 
-app.use(morgan("dev"));
+app.use(express.json());
+
+morgan.token("reqData", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqData"
+  )
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -26,12 +35,30 @@ app.get("/api/recipes/:id", (req, res) => {
   }
 });
 
+app.post("/api/recipes", (req, res) => {
+  const maxId =
+    recipes.length > 0 ? Math.max(...recipes.map((n) => Number(n.id))) : 0;
+
+  const recipe = req.body;
+  recipe.id = String(maxId + 1);
+
+  recipes = recipes.concat(recipe);
+
+  res.json(recipe);
+});
+
 app.delete("/api/recipes/:id", (req, res) => {
   const id = req.params.id;
   recipes = recipes.filter((recipe) => recipe.id !== id);
 
   res.status(204).end();
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
